2e3c5a5f096217a4b4b6ac34916c78edff52d82a
ahrtechandseries-wq <ahrtechandseries@gmail.com>
2026-04-18 23:11:55 +0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'nexflix-v1';
+const assets = ['/', 'https://www.rnexflix.top/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
